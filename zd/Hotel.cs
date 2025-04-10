b4ff376d97ef485123a3942d78e4ac4acfbf5b85
IvanSakuta <vanasakuta@gmail.com>
2025-04-10 09:25:16 +0300

    add hotel

---
+++ zd/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd
+{
+    public class Hotel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int CountOfStars { get; set; }
+        public string CountryName { get; set; }
+        public string HotelImage { get; set; }
+
+    }
+}
